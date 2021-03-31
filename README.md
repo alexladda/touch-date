41b02fc7bfa3b05b18c328cb634e6f13d00be721
Alex Ladda <alex.ladda@gmail.com>
2021-03-31 20:18:15 +0800

    script now works on simple data set of exported jpg images:
    > python touch-date.py path-to-media

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # A Script to correct the date timestamps when exporting from OS X Photos App
 
+When exporting from Photos App in OS X the original timestamp is not preserved. This script restores the original timestamp for Media exported from the Photos Library.
+
 1. in Photos: "File > Export unmodified Originals ..."
 2. select checkbox for XML files
 3. run Script: python3 touch-date.py path-to-files
@@ -7,4 +9,4 @@
   - find matching pairs of media file and xmp sidecar
   - extract data from xmp sidecar
   - touch media file with timestamp
-4. enjoy.
+4. relax.
--- touch-date.py
+++ touch-date.py
@@ -11,6 +11,47 @@ Syntax:
 
 """
 
+
 import sys
+import os
+import xml.etree.ElementTree as ET
+import subprocess
+
+
+def get_timestamp(file):
+    tree = ET.parse(file)
+    root = tree.getroot()
+    extracted_timestamp = root[0][0][12].text
+    return extracted_timestamp
+
+
+def prep_timestamp(extracted_timestamp):
+    """
+    input has this format: 2021-03-21T19:50:28+08:00
+    returns this format: 20210321195028
+    """
+    # TODO: preserve timezone info?
+    # TODO: automatically detect integers
+    # TODO: integrate in or call from get_timestamp() function
+    modified_timestamp = extracted_timestamp[:-6]
+    unwanted = ['-', 'T', ':']
+    for item in range(len(unwanted)):
+        modified_timestamp = modified_timestamp.replace(unwanted[item], '')
+    modified_timestamp = modified_timestamp[:-2] + '.' + modified_timestamp[-2:]
+    return modified_timestamp
+
+
+def touch_date(file, timestamp):
+    shellcommand = ["touch", "-t", timestamp, file]
+    subprocess.run(shellcommand)
+
 
 path = sys.argv[1]
+
+for dirpath, dirnames, filenames in os.walk(path):
+    for filename in filenames:
+        if os.path.splitext(filename)[1] == ".xmp":
+            fullpath = os.path.join(os.getcwd(), dirpath, filename)
+            timestamp = get_timestamp(fullpath)
+            timestamp = prep_timestamp(timestamp)
+            touch_date(fullpath, timestamp)
