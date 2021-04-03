f9cc531847edc6018ba11af5ec560dbc64998904
Alex Ladda <alex.ladda@gmail.com>
2021-04-03 16:06:58 +0800

    working script expanded README.md

--- README.md
+++ README.md
@@ -1,12 +1,47 @@
 # A Script to correct the date timestamps when exporting from OS X Photos App
 
-When exporting from Photos App in OS X the original timestamp is not preserved. This script restores the original timestamp for Media exported from the Photos Library.
+## Which problem does this Script solve?
 
-1. in Photos: "File > Export unmodified Originals ..."
-2. select checkbox for XML files
-3. run Script: python3 touch-date.py path-to-files
-  - Walk through directory
-  - find matching pairs of media file and xmp sidecar
-  - extract data from xmp sidecar
-  - touch media file with timestamp
-4. relax.
+When exporting from Photos App in OS X the original timestamp is not preserved. Instead a new timestamp (as in "now") is created for the new file.
+
+I assume no one has ever been bothered by this. I can understand that one could argue the timestamp of the file should relate to the creation date of the file and not of the Photo.
+
+But this has always bothered me. So that's why I wrote this Script.
+
+## What does this Script do exactly?
+
+This script restores the original timestamp to the media files after they are exported from the Photos Library:
+
+- search the specified folder (and all subfolders) for xmp sidecar files
+- extract their timestamps
+- set the respective file's 'Date Created' and 'Date modified' to the value found in the xmp sidecar file
+
+## Usage
+
+### 1. Export
+- in Photos: "File > Export unmodified Originals ..."
+- select checkbox for XML files
+- export to folder
+
+### 2. run Script
+
+`python3 touch-date.py <path to files>`
+
+or
+
+`./touch-date.py <path to files>`
+
+
+- scan the specified folder (and all subfolders) for xmp sidecar files
+- extract the timestamps
+- if theres corresponding media files (photos or videos) it sets their 'Date Created' and 'Date modified' to the value found in the xmp sidecar file
+
+
+***Word of caution:***
+
+This might not work on your machine, could mess up things if used in the wrong way.
+This has not been tested beyond a few files on my drive.
+
+- Mac OS 11.2.3 (Big Sur)
+- Photos 6.0
+- Python 3.9.2
--- touch-date.py
+++ touch-date.py
@@ -6,52 +6,95 @@
 3. extract data from xmp sidecar
 4. touch media file with timestamp
 
-Syntax:
-> touch-date.py path-to-media
+Usage:
+> python touch-date.py path-to-media
+or:
+> ./ touch-date.py path-to-media
 
+This might not work on your machine, could mess up things if used in the wrong way.
+This has not been tested beyond a few files on my drive.
+Mac OS 11.2.3 (Big Sur)
+Photos 6.0
+Python 3.9.2
 """
 
 
-import sys
-import os
-import xml.etree.ElementTree as ET
-import subprocess
+import sys                          # get command line argument
+import os                           # access files
+import xml.etree.ElementTree as ET  # XML Parser
+import subprocess                   # run Bash command
 
 
 def get_timestamp(file):
+    # parse XML file, Element Tree object
     tree = ET.parse(file)
+    # get root Element (?)
     root = tree.getroot()
-    extracted_timestamp = root[0][0][12].text
-    return extracted_timestamp
+    # iterate through element tree
+    for elem in root.iter():
+        # find Date Tag
+        if elem.tag.endswith("DateCreated"):
+            # return text string of said Element
+            return elem.text
 
 
-def prep_timestamp(extracted_timestamp):
+def prep_timestamp(xmp_timestamp):
     """
-    input has this format: 2021-03-21T19:50:28+08:00
-    returns this format: 20210321195028
+    input (xmp_timestamp) has this format: 2021-03-21T19:50:28+08:00
+    returns (timestamp) this format: 202103211950.28
     """
     # TODO: preserve timezone info?
     # TODO: automatically detect integers
     # TODO: integrate in or call from get_timestamp() function
-    modified_timestamp = extracted_timestamp[:-6]
+
+    # cut off time zone
+    timestamp = xmp_timestamp[:-6]
+    # cut off unwanted characters
     unwanted = ['-', 'T', ':']
     for item in range(len(unwanted)):
-        modified_timestamp = modified_timestamp.replace(unwanted[item], '')
-    modified_timestamp = modified_timestamp[:-2] + '.' + modified_timestamp[-2:]
-    return modified_timestamp
+        timestamp = timestamp.replace(unwanted[item], '')
+    # add a '.' at the appropriate position
+    timestamp = timestamp[:-2] + '.' + timestamp[-2:]
+    return timestamp
 
 
-def touch_date(file, timestamp):
-    shellcommand = ["touch", "-t", timestamp, file]
-    subprocess.run(shellcommand)
+def touch_date(timestamp, cwd, dirpath, filename):
+    """
+    This function takes a timestamp and the path to an xmp sidecar file.
+    It will then touch all corresponding files and said xmp file with
+    the specified timestamp.
+    """
+    # xmppath is the absolute file path of the xmp sidecar file:
+    # /Users/<user>/export/IMG_1234.xmp
+
+    # We want to run something like the following command to touch
+    # all corresponding files:
+    # Bash $> touch -t <timestamp> /Users/<user>/export/IMG_1234.*
+
+    # assembling the argument for the touch command
+    wildcardname = filename[:-4] + '.*'
+    wildcardpath = os.path.join(cwd, dirpath, wildcardname)
+    wildcardpath = wildcardpath.replace(" ", "\\ ")
+    print("wildcardpath:", wildcardpath)
+    shellcommand = 'touch -t {0} {1}'.format(timestamp, wildcardpath)
+
+    # giving some feedback
+    print(shellcommand)
+    # running the shell command
+    subprocess.run(shellcommand, shell=True)
 
 
-path = sys.argv[1]
+# getting directory from command line argument
+search_dir = sys.argv[1]
 
-for dirpath, dirnames, filenames in os.walk(path):
+# walking search directory
+for dirpath, dirnames, filenames in os.walk(search_dir):
     for filename in filenames:
+        # check for xmp sidecar files
         if os.path.splitext(filename)[1] == ".xmp":
-            fullpath = os.path.join(os.getcwd(), dirpath, filename)
-            timestamp = get_timestamp(fullpath)
+            # get the full path of .xmp files
+            xmppath = os.path.join(os.getcwd(), dirpath, filename)
+            # get and prep timestamp from xmp file
+            timestamp = get_timestamp(xmppath)
             timestamp = prep_timestamp(timestamp)
-            touch_date(fullpath, timestamp)
+            touch_date(timestamp, os.getcwd(), dirpath, filename)
